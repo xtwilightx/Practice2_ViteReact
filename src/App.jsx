be17dcb7d45cf7f2dfc71ac8276c719a388f3dca
Xidi <xidi234@mail.ru>
2025-07-14 09:06:21 +0300

    add card icons for game; add basic arrays for game

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import GameBoard from './Components/GameBoard'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import { CpuChipIcon } from '@heroicons/react/24/solid'
+import { AcademicCapIcon } from '@heroicons/react/16/solid'
+import { LanguageIcon } from '@heroicons/react/16/solid'
+import { CakeIcon } from '@heroicons/react/16/solid'
+import { ScissorsIcon } from '@heroicons/react/16/solid'
+
+import { QuestionMarkCircleIcon } from '@heroicons/react/16/solid'
+
+const initialArrayCards = [
+  {id: 1, img: BeakerIcon},
+  {id: 2, img: CpuChipIcon},
+  {id: 3, img: AcademicCapIcon},
+  {id: 4, img: LanguageIcon},
+  {id: 5, img: CakeIcon},
+  {id: 6, img: ScissorsIcon},
+]
 
 
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
-  const numbers = [1,2,3,4,5];
-  const doubled = numbers.map((number) => number * 2);
-  console.log(doubled);
+  const [arrayCards, setArrayCards] = useState([]) //Закрытые карточки
+  const [openCards, setOpenCards] = useState([]) //Открытые карточки
 
-  return (
-    <>
-      <GameBoard />
-    </>
+  const [matched, setMatched] = useState([]) //Совпадения
+  const [moves, setMoves] = useState([]) //Шаги
+
+  const paitOfArrayCards = [...initialArrayCards, ...initialArrayCards] //2 массива карточек
+
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+
+    while (currentIndex !== 0){
+      randomIndex = Math.floor (Math.random() * currentIndex)
+      currentIndex -= 1
+
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
+    
+    }
+    return array
+  }
+
+
+  return(
+    <div className='App'>
+  
+    </div>
+    
   )
 }
 
--- src/ui/GameCard.jsx
+++ src/ui/GameCard.jsx
@@ -1,5 +1,5 @@
 import Card from '../assets/images/Card.png'
-import { BeakerIcon } from '@heroicons/react/24/solid'
+
 
 export const GameCard = ({photo}) => 
 <button className='size-36'><img src={Card}></img></button>
