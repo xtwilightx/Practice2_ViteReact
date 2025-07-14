ecaf2213fd90c577b52d2f844b765d375838d8a3
Xidi <xidi234@mail.ru>
2025-07-14 11:35:08 +0300

    cards now display in the game

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,15 @@ const initialArrayCards = [
   {id: 6, img: ScissorsIcon},
 ]
 
+const paitOfArrayCards = [...initialArrayCards, ...initialArrayCards]
 
 const App = () => {
 
   const [arrayCards, setArrayCards] = useState([]) //Закрытые карточки
-  const [openCards, setOpenCards] = useState([]) //Открытые карточки
+  const [openedCards, setOpenCards] = useState([]) //Открытые карточки
 
   const [matched, setMatched] = useState([]) //Совпадения
   const [moves, setMoves] = useState([]) //Шаги
-
-  const paitOfArrayCards = [...initialArrayCards, ...initialArrayCards] //2 массива карточек
 
   const shuffle = (array) => {
     let currentIndex = array.length,
@@ -39,7 +38,7 @@ const App = () => {
 
 
     while (currentIndex !== 0){
-      randomIndex = Math.floor (Math.random() * currentIndex)
+      randomIndex = Math.floor(Math.random() * currentIndex)
       currentIndex -= 1
 
       temporaryValue = array[currentIndex]
@@ -50,10 +49,39 @@ const App = () => {
     return array
   }
 
+    useEffect(() =>{
+      setArrayCards(shuffle(paitOfArrayCards))
+    },[]) //[зависимости] Пусто значит при загрузке страницы сработает
 
   return(
     <div className='App'>
-  
+        {console.log({arrayCards})}
+        <p className='number-of-strokes'>Сделано ходов {moves}</p>
+        <div className='cards'>
+            {arrayCards.map((item, index, number) => {
+              //Вынести в компонент
+              let isFlipped = false;
+
+              if (openedCards.includes(index)) isFlipped = true
+              if (matched.includes(item.id)) isFlipped = true
+              
+              //лучше не index
+              return(
+                <div key={index} className={`card ${isFlipped ? 'flipped' : ''}`}> 
+                  <div>
+                    <div>
+                      <item.img className='size-24'  />
+                    </div>
+
+                    <div>
+                      <QuestionMarkCircleIcon className='size-24' />
+                    </div>
+                  </div>
+                </div>
+              )
+
+          } )}
+        </div>
     </div>
     
   )
