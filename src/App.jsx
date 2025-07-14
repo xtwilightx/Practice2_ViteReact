cdf8f775e815d3f146be82216b2c86a8675132f5
xtwilightx <iqxidi@gmail.com>
2025-07-14 15:22:04 +0300

    fix cards position

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -13,12 +13,12 @@ import { ScissorsIcon } from '@heroicons/react/16/solid'
 import { QuestionMarkCircleIcon } from '@heroicons/react/16/solid'
 
 const initialArrayCards = [
-  {id: 1, img: BeakerIcon},
-  {id: 2, img: CpuChipIcon},
-  {id: 3, img: AcademicCapIcon},
-  {id: 4, img: LanguageIcon},
-  {id: 5, img: CakeIcon},
-  {id: 6, img: ScissorsIcon},
+  { id: 1, img: BeakerIcon },
+  { id: 2, img: CpuChipIcon },
+  { id: 3, img: AcademicCapIcon },
+  { id: 4, img: LanguageIcon },
+  { id: 5, img: CakeIcon },
+  { id: 6, img: ScissorsIcon },
 ]
 
 const paitOfArrayCards = [...initialArrayCards, ...initialArrayCards]
@@ -33,58 +33,52 @@ const App = () => {
 
   const shuffle = (array) => {
     let currentIndex = array.length,
-    temporaryValue,
-    randomIndex
+      temporaryValue,
+      randomIndex
 
 
-    while (currentIndex !== 0){
+    while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex)
+      console.log(randomIndex);
       currentIndex -= 1
 
       temporaryValue = array[currentIndex]
       array[currentIndex] = array[randomIndex]
       array[randomIndex] = temporaryValue
-    
+
     }
     return array
   }
 
-    useEffect(() =>{
-      setArrayCards(shuffle(paitOfArrayCards))
-    },[]) //[зависимости] Пусто значит при загрузке страницы сработает
+  useEffect(() => {
+    setArrayCards(shuffle(paitOfArrayCards))
+  }, []) 
 
-  return(
-    <div className='App'>
-        {console.log({arrayCards})}
-        <p className='number-of-strokes'>Сделано ходов {moves}</p>
-        <div className='cards'>
-            {arrayCards.map((item, index, number) => {
-              //Вынести в компонент
-              let isFlipped = false;
+return (
+  <div className='App'>
+    <p className='number-of-strokes'>Сделано ходов {moves}</p>
+    <div className='cards flex flex-wrap justify-center gap-4'> 
+      {arrayCards.map((item, index) => {
+        const isFlipped = openedCards.includes(index) || matched.includes(item.id);
 
-              if (openedCards.includes(index)) isFlipped = true
-              if (matched.includes(item.id)) isFlipped = true
-              
-              //лучше не index
-              return(
-                <div key={index} className={`card ${isFlipped ? 'flipped' : ''}`}> 
-                  <div>
-                    <div>
-                      <item.img className='size-24'  />
-                    </div>
+        return (
+          <div 
+            key={index} 
+            className={`card relative w-32 h-32 ${isFlipped ? 'flipped' : ''}`} >
+            <div className="w-full h-full flex items-center justify-center">
+              <item.img className='size-20 text-white rounded-xl bg-slate-400' />
+            </div>
 
-                    <div>
-                      <QuestionMarkCircleIcon className='size-24' />
-                    </div>
-                  </div>
-                </div>
-              )
-
-          } )}
-        </div>
+            <div className={`absolute inset-0 flex items-center justify-center 
+                             ${isFlipped ? 'hidden' : ''}`}> 
+              <QuestionMarkCircleIcon className='size-20 text-white rounded-xl bg-slate-400' />
+            </div>
+          </div>
+        );
+      })}
     </div>
-    
-  )
+  </div>
+);
 }
 
 export default App
